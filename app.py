bb8689d512bf7080e462e79876ec5430cbd48bf2
Ovindu Wijethunge <oumw.udesh@gmail.com>
2021-05-03 18:01:40 +0530

    spam id predicted and showed in web

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ from sklearn.preprocessing import StandardScaler
 
 
 app = Flask(__name__)
-#model = pickle.load(open('xgboost_Classifier.pkl', 'rb'))
+model = pickle.load(open('dt.pkl', 'rb'))
+scaler = pickle.load(open('scaler.pkl', 'rb'))
 
 @app.route('/')
 def home():
@@ -23,21 +24,20 @@ def predict():
     For rendering results on HTML GUI
     '''
     id = [x for x in request.form.values()]
-    #final_features = [np.array(int_features)]
     val = id[0] 
     str_val = str(val)
-    download_comments_and_content(str_val)
+    #download_comments_and_content(str_val)-----
     
-    #df = pd.read_csv('data.csv')
-    #df = df.drop('comment_id',axis =1)
+    df = pd.read_csv('data.csv')
+    X = df.drop('cid',axis =1)
     
     #scaler = StandardScaler()
     #scaler.fit(df)
-    #df = scaler.transform(df)
+    X_scaled = scaler.transform(X)
     #input_list = df.values.tolist()
     
-    #prediction = model.predict(df)
-
+    prediction = model.predict(X_scaled)
+    print(prediction)
     #output = round(prediction[0], 2)
     #x = [-0.501935,-0.0559087,-0.0161989,-0.297034,-0.601665,-0.235129,-0.267696,-0.372999,-0.622836,-0.407599,1.26674,-0.58052,-0.328634]
     #y = [2.18214,-0.813473,-0.453712,-0.370705,-0.609161,-0.356038,-0.37072,-0.37333,-0.614514,-0.405674,-0.880783,-0.585333,-0.32857]
@@ -48,20 +48,20 @@ def predict():
     
     # concatinate the output values to comment excel file
     #comment_file = pd.read_excel('commentData.xlsx')
-    #prediction_series = pd.Series(prediction)
-    #comment_file = pd.concat([comment_file,prediction_series], axis=1)
-    #comment_file = comment_file.rename(columns={0: 'classifier'})
-    #id_group = comment_file.groupby(['classifier'])
-    #spam_group = id_group.get_group(1)
-    #spam_comments_ids = spam_group['comment_id']
+    prediction_series = pd.Series(prediction)
+    df1= pd.concat([df,prediction_series], axis=1)
+    df1 = df1.rename(columns={0: 'is_spam'})
+    id_group = df1.groupby(['is_spam'])
+    spam_group = id_group.get_group(1)
+    spam_list = spam_group['cid'].tolist()
     
     #spam_id_list = spam_comments_ids.tolist()
     #string_ids = [str(li) for li in spam_id_list]
     
     #get_ham_comments(str_val,string_ids)
     #return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(prediction))
-    #return render_template('index.html', prediction_text=prediction)
-    return render_template('index.html', prediction_text="download success")
+    return render_template('index.html', prediction_text=spam_list)
+    #return render_template('index.html', prediction_text="download success")
 
 # =============================================================================
 # @app.route('/predict_api',methods=['POST'])
